d97c83af096ef1301dc890cfb3bbf9be076e224c
doodeec <doodeec@gmail.com>
2014-02-22 17:53:33 +0100

    [test] blog service

---
+++ test/spec/blog/blog.js
@@ -0,0 +1,58 @@
+'use strict';
+
+describe('Blog:: BlogService', function () {
+
+    // load the controller's module
+    beforeEach(module('dc-blog'));
+
+    var BlogService,
+        $httpBackend,
+        articles;
+
+    // Initialize the controller and a mock scope
+    beforeEach(inject(function (_$httpBackend_, _BlogService_) {
+        BlogService = _BlogService_;
+        $httpBackend = _$httpBackend_;
+    }));
+
+    afterEach(function () {
+        $httpBackend.flush();
+    });
+
+    it('loadBlog: should load specific blog with id 5', function () {
+        $httpBackend.expectGET('/api/blog?id=5')
+            .respond({id: 5, title: 'Blog 5'});
+
+        BlogService.loadBlog(5).then(function (response) {
+            expect(response.data).toBeDefined();
+            expect(response.data.id).toBe(5);
+        });
+    });
+
+    it('loadBlogs: should load 2 blogs', function () {
+        $httpBackend.expectGET('/api/blogs/home')
+            .respond([
+                {id: 1, title: 'Blog 1'},
+                {id: 2, title: 'Blog 5'}
+            ]);
+
+        BlogService.loadBlogs().then(function (response) {
+            expect(response.data).toBeDefined();
+            expect(response.data.length).toBe(2);
+        });
+    });
+
+    it('loadAllBlogs: should load 3 blogs', function () {
+        $httpBackend.expectGET('/api/blogs')
+            .respond([
+                {id: 1, title: 'Blog 1'},
+                {id: 2, title: 'Blog 2'},
+                {id: 2, title: 'Blog 5'}
+            ]);
+
+        BlogService.loadAllBlogs().then(function (response) {
+            expect(response.data).toBeDefined();
+            expect(response.data.length).toBe(3);
+        });
+    });
+});
--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -6,23 +6,43 @@ describe('Controller: MainCtrl', function () {
     beforeEach(module('dc21App'));
 
     var MainCtrl,
+        BlogService,
         scope,
         $httpBackend;
 
     // Initialize the controller and a mock scope
-    beforeEach(inject(function (_$httpBackend_, $controller, $rootScope) {
+    beforeEach(inject(function (_$httpBackend_, _BlogService_, $controller, $rootScope) {
         $httpBackend = _$httpBackend_;
-        $httpBackend.expectGET('/api/awesomeThings')
-            .respond(['HTML5 Boilerplate', 'AngularJS', 'Karma', 'Express']);
+        BlogService = _BlogService_;
+
+        $httpBackend.expectGET('/api/blogs/home')
+            .respond([
+                {id: 1, title: 'Blog 1'},
+                {id: 2, title: 'Blog 5'}
+            ]);
+
+        $httpBackend.expectGET('/api/blogs')
+            .respond([
+                {id: 1, title: 'Blog 1'},
+                {id: 2, title: 'Blog 2'},
+                {id: 2, title: 'Blog 5'}
+            ]);
+
         scope = $rootScope.$new();
         MainCtrl = $controller('MainCtrl', {
-            $scope: scope
+            $scope: scope, BlogService: BlogService
         });
     }));
 
-    it('should attach a list of awesomeThings to the scope', function () {
-        expect(scope.awesomeThings).toBeUndefined();
+    it('should attach a list of 2 blogs to the scope.blogArticles', function () {
+        expect(scope.blogArticles).toBeUndefined();
         $httpBackend.flush();
-        expect(scope.awesomeThings.length).toBe(4);
+        expect(scope.blogArticles.length).toBe(2);
+    });
+
+    it('should attach a list of 3 blogs to the scope.allBlogs', function () {
+        expect(scope.allBlogs).toBeUndefined();
+        $httpBackend.flush();
+        expect(scope.allBlogs.length).toBe(3);
     });
 });
