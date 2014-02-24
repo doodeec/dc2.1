6bfaca05b5dc74930bb167563b46b769ef499124
doodeec <doodeec@gmail.com>
2014-02-24 17:35:46 +0100

    admin service + unit tests

--- app/scripts/admin/admin.js
+++ app/scripts/admin/admin.js
@@ -2,14 +2,20 @@
 
 angular.module('dc-admin', [])
     .factory('Admin', function ($http) {
+        var errorStrings = {
+            misProjId: 'Missing project id',
+            misBlogId: 'Missing blog id',
+            typeError: 'Wrong parameter type'
+        };
+
         return {
             /**
              * Loads blog with defined id
-             * @param {String} id - blog id
+             * @param {String} id
              * @returns {Object} $http promise
              */
             loadBlog: function (id) {
-                if (!angular.isDefined(id)) throw new Error('Missing blog id');
+                if (!angular.isDefined(id)) throw new Error(errorStrings.misBlogId);
                 return $http.get('/api/blog', {params: {id: id}});
             },
             /**
@@ -25,7 +31,7 @@ angular.module('dc-admin', [])
              * @returns {Object} $http promise
              */
             createBlog: function (params) {
-                if (!angular.isObject(params)) throw new Error('Wrong parameter type');
+                if (!angular.isObject(params)) throw new Error(errorStrings.typeError);
                 return $http.post('/api/blog/create', params);
             },
             /**
@@ -34,7 +40,7 @@ angular.module('dc-admin', [])
              * @returns {Object} $http promise
              */
             editBlog: function (data) {
-                if (!angular.isObject(data)) throw new Error('Wrong parameter type');
+                if (!angular.isObject(data)) throw new Error(errorStrings.typeError);
                 return $http.post('/api/blog/save', data);
             },
             /**
@@ -43,18 +49,62 @@ angular.module('dc-admin', [])
              * @returns {Object} $http promise
              */
             deleteBlog: function (id) {
-                if (!angular.isDefined(id)) throw new Error('Missing blog id');
+                if (!angular.isDefined(id)) throw new Error(errorStrings.misBlogId);
                 return $http.post('/api/blog/delete', id);
             },
-            publishBlog: function () {
+            /**
+             * Publishes blog into home page
+             * @param {String} id
+             * @returns {Object} $http promise
+             */
+            publishBlog: function (id) {
+                if (!angular.isDefined(id)) throw new Error(errorStrings.misBlogId);
+                return this.editBlog({id: id, allowedHome: true});
             },
-            loadProject: function () {
+            /**
+             * Unpublishes blog from home page
+             * @param {String} id
+             * @returns {Object} $http promise
+             */
+            unpublishBlog: function (id) {
+                if (!angular.isDefined(id)) throw new Error(errorStrings.misBlogId);
+                return this.editBlog({id: id, allowedHome: false});
             },
-            createProject: function () {
+            /**
+             * Loads project with specific id
+             * @param {String} id
+             * @returns {Object} $http promise
+             */
+            loadProject: function (id) {
+                if (!angular.isDefined(id)) throw new Error(errorStrings.misProjId);
+                return $http.get('/api/project', {params: {id: id}});
             },
-            editProject: function () {
+            /**
+             * Creates new project
+             * @param {Object} params - new project properties
+             * @returns {Object} $http promise
+             */
+            createProject: function (params) {
+                if (!angular.isObject(params)) throw new Error(errorStrings.typeError);
+                return $http.post('/api/project/create', params);
             },
-            deleteProject: function () {
+            /**
+             * Updates existing project with
+             * @param {Object} data - changed project properties and project id
+             * @returns {Object} $http promise
+             */
+            editProject: function (data) {
+                if (!angular.isObject(data)) throw new Error(errorStrings.typeError);
+                return $http.post('/api/project/save', data);
+            },
+            /**
+             * Deletes project with specific id
+             * @param {String} id
+             * @returns {Object} $http promise
+             */
+            deleteProject: function (id) {
+                if (!angular.isDefined(id)) throw new Error(errorStrings.misBlogId);
+                return $http.post('/api/project/delete', id);
             }
         };
     });
--- test/spec/admin/admin.js
+++ test/spec/admin/admin.js
@@ -13,171 +13,186 @@ describe('Admin:: Admin', function () {
         $httpBackend = _$httpBackend_;
     }));
 
-    afterEach(function () {
-        $httpBackend.flush();
-    });
-
     it('should have all methods defined', function () {
         expect(AdminService.loadBlog).toBeDefined();
         expect(AdminService.loadAllBlogs).toBeDefined();
         expect(AdminService.createBlog).toBeDefined();
         expect(AdminService.editBlog).toBeDefined();
         expect(AdminService.deleteBlog).toBeDefined();
+        expect(AdminService.unpublishBlog).toBeDefined();
         expect(AdminService.publishBlog).toBeDefined();
         expect(AdminService.createProject).toBeDefined();
         expect(AdminService.editProject).toBeDefined();
-
-        //TODO $http flush not needed
     });
 
-    describe('method:: loadBlog', function () {
-        beforeEach(function () {
-            $httpBackend.expectGET('/api/blog?id=12')
-                .respond({ id: 12 });
+    describe('Admin.methods', function () {
+        afterEach(function () {
+            $httpBackend.flush();
         });
 
-        it('should load one blog', function () {
-            AdminService.loadBlog(12).then(function (response) {
-                expect(response.data).toBeDefined();
-                expect(response.data.id).toBe(12);
+        describe('method:: loadBlog', function () {
+            beforeEach(function () {
+                $httpBackend.expectGET('/api/blog?id=12')
+                    .respond({ id: 12 });
+            });
+
+            it('should load one blog', function () {
+                AdminService.loadBlog(12).then(function (response) {
+                    expect(response.data).toBeDefined();
+                    expect(response.data.id).toBe(12);
+                });
             });
         });
-    });
 
-    describe('method:: loadAllBlogs', function () {
-        beforeEach(function () {
-            $httpBackend.expectGET('/api/blogs')
-                .respond([
-                    { id: 1 },
-                    { id: 2 },
-                    { id: 5 }
-                ]);
-        });
+        describe('method:: loadAllBlogs', function () {
+            beforeEach(function () {
+                $httpBackend.expectGET('/api/blogs')
+                    .respond([
+                        { id: 1 },
+                        { id: 2 },
+                        { id: 5 }
+                    ]);
+            });
 
-        it('should load an array of all blogs', function () {
-            AdminService.loadAllBlogs().then(function (response) {
-                expect(response.data).toBeDefined();
-                expect(response.data.length).toBe(3);
+            it('should load an array of all blogs', function () {
+                AdminService.loadAllBlogs().then(function (response) {
+                    expect(response.data).toBeDefined();
+                    expect(response.data.length).toBe(3);
+                });
             });
         });
-    });
 
-    describe('method:: createBlog', function () {
-        beforeEach(function () {
-            $httpBackend.expectPOST('/api/blog/create')
-                .respond(200);
-        });
+        describe('method:: createBlog', function () {
+            beforeEach(function () {
+                $httpBackend.expectPOST('/api/blog/create')
+                    .respond(200);
+            });
 
-        it('should send a post request to /api/blog/create', function () {
-            var params = { id: 12 };
+            it('should send a post request to /api/blog/create', function () {
+                var params = { id: 12 };
 
-            AdminService.createBlog(params).then(function (response) {
-                expect(response).toBeDefined();
-                expect(response.status).toBe(200);
+                AdminService.createBlog(params).then(function (response) {
+                    expect(response).toBeDefined();
+                    expect(response.status).toBe(200);
+                });
             });
         });
-    });
 
-    describe('method:: editBlog', function () {
-        beforeEach(function () {
-            $httpBackend.expectPOST('/api/blog/save')
-                .respond(200);
-        });
+        describe('method:: editBlog', function () {
+            beforeEach(function () {
+                $httpBackend.expectPOST('/api/blog/save')
+                    .respond(200);
+            });
 
-        it('should send a post request to /api/blog/save', function () {
-            var params = { id: 12 };
+            it('should send a post request to /api/blog/save', function () {
+                var params = { id: 12 };
 
-            AdminService.editBlog(params).then(function (response) {
-                expect(response).toBeDefined();
-                expect(response.status).toBe(200);
+                AdminService.editBlog(params).then(function (response) {
+                    expect(response).toBeDefined();
+                    expect(response.status).toBe(200);
+                });
             });
         });
-    });
 
-    describe('method:: deleteBlog', function () {
-        beforeEach(function () {
-            $httpBackend.expectPOST('/api/blog/delete')
-                .respond(200);
-        });
+        describe('method:: deleteBlog', function () {
+            beforeEach(function () {
+                $httpBackend.expectPOST('/api/blog/delete')
+                    .respond(200);
+            });
 
-        it('should send a post request to /api/blog/delete', function () {
-            AdminService.deleteBlog(12).then(function (response) {
-                expect(response).toBeDefined();
-                expect(response.status).toBe(200);
+            it('should send a post request to /api/blog/delete', function () {
+                AdminService.deleteBlog(12).then(function (response) {
+                    expect(response).toBeDefined();
+                    expect(response.status).toBe(200);
+                });
             });
         });
-    });
 
-    describe('method:: publishBlog', function () {
-        beforeEach(function () {
-            $httpBackend.expectPOST('/api/blog/save')
-                .respond(200);
-        });
+        describe('method:: publishBlog', function () {
+            beforeEach(function () {
+                $httpBackend.expectPOST('/api/blog/save')
+                    .respond(200);
+            });
 
-        it('should send a post request to /api/blog/save', function () {
-            AdminService.publishBlog(12).then(function (response) {
-                expect(response).toBeDefined();
-                expect(response.status).toBe(200);
+            it('should send a post request to /api/blog/save', function () {
+                AdminService.publishBlog(12).then(function (response) {
+                    expect(response).toBeDefined();
+                    expect(response.status).toBe(200);
+                });
             });
         });
-    });
 
-    describe('method:: loadProject', function () {
-        beforeEach(function () {
-            $httpBackend.expectGET('/api/project?id=2')
-                .respond({ id: 2 });
-        });
+        describe('method:: unpublishBlog', function () {
+            beforeEach(function () {
+                $httpBackend.expectPOST('/api/blog/save')
+                    .respond(200);
+            });
 
-        it('should load one project', function () {
-            AdminService.loadProject(2).then(function (response) {
-                expect(response.data).toBeDefined();
-                expect(response.data.id).toBe(2);
+            it('should send a post request to /api/blog/save', function () {
+                AdminService.unpublishBlog(12).then(function (response) {
+                    expect(response).toBeDefined();
+                    expect(response.status).toBe(200);
+                });
             });
         });
-    });
 
-    describe('method:: createProject', function () {
-        beforeEach(function () {
-            $httpBackend.expectPOST('/api/project/create')
-                .respond(200);
-        });
+        describe('method:: loadProject', function () {
+            beforeEach(function () {
+                $httpBackend.expectGET('/api/project?id=2')
+                    .respond({ id: 2 });
+            });
 
-        it('should send a post request to /api/project/create', function () {
-            var params = { id: 12 };
-
-            AdminService.createProject(params).then(function (response) {
-                expect(response).toBeDefined();
-                expect(response.status).toBe(200);
+            it('should load one project', function () {
+                AdminService.loadProject(2).then(function (response) {
+                    expect(response.data).toBeDefined();
+                    expect(response.data.id).toBe(2);
+                });
             });
         });
-    });
 
-    describe('method:: editProject', function () {
-        beforeEach(function () {
-            $httpBackend.expectPOST('/api/project/save')
-                .respond(200);
-        });
+        describe('method:: createProject', function () {
+            beforeEach(function () {
+                $httpBackend.expectPOST('/api/project/create')
+                    .respond(200);
+            });
 
-        it('should send a post request to /api/project/save', function () {
-            var params = { id: 12 };
+            it('should send a post request to /api/project/create', function () {
+                var params = { id: 12 };
 
-            AdminService.editProject(params).then(function (response) {
-                expect(response).toBeDefined();
-                expect(response.status).toBe(200);
+                AdminService.createProject(params).then(function (response) {
+                    expect(response).toBeDefined();
+                    expect(response.status).toBe(200);
+                });
             });
         });
-    });
 
-    describe('method:: deleteProject', function () {
-        beforeEach(function () {
-            $httpBackend.expectPOST('/api/project/delete')
-                .respond(200);
+        describe('method:: editProject', function () {
+            beforeEach(function () {
+                $httpBackend.expectPOST('/api/project/save')
+                    .respond(200);
+            });
+
+            it('should send a post request to /api/project/save', function () {
+                var params = { id: 12 };
+
+                AdminService.editProject(params).then(function (response) {
+                    expect(response).toBeDefined();
+                    expect(response.status).toBe(200);
+                });
+            });
         });
 
-        it('should send a post request to /api/project/delete', function () {
-            AdminService.deleteProject(12).then(function (response) {
-                expect(response).toBeDefined();
-                expect(response.status).toBe(200);
+        describe('method:: deleteProject', function () {
+            beforeEach(function () {
+                $httpBackend.expectPOST('/api/project/delete')
+                    .respond(200);
+            });
+
+            it('should send a post request to /api/project/delete', function () {
+                AdminService.deleteProject(12).then(function (response) {
+                    expect(response).toBeDefined();
+                    expect(response.status).toBe(200);
+                });
             });
         });
     });
