09ca3c4c3a8ae158c1bea6b8375ae6d5f2c32abd
doodeec <doodeec@gmail.com>
2014-03-26 10:29:17 +0100

    socket service

--- app/scripts/socket/socket.js
+++ app/scripts/socket/socket.js
@@ -2,25 +2,23 @@
 
 angular.module('dc-socket', [])
     .factory('Socket', function ($window) {
-        if (angular.isUndefined($window.io)) {
-            return {
-                on: angular.noop,
-                emit: angular.noop,
-                removeListener: angular.noop
-            }
+        var on = angular.noop,
+            emit = angular.noop,
+            removeListener = angular.noop;
+
+        if (angular.isDefined($window.io)) {
+            var socket = $window.io.connect();
+
+            on = socket.on;
+            emit = socket.emit;
+            removeListener = function () {
+                return socket.removeListener.apply(socket, arguments);
+            };
         }
 
-        var socket = $window.io.connect();
-
         return {
-            on: function (event, callback) {
-                socket.on(event, callback);
-            },
-            emit: function (event, data) {
-                return socket.emit(event, data);
-            },
-            removeListener: function () {
-                return socket.removeListener.apply(socket, arguments);
-            }
+            on: on,
+            emit: emit,
+            removeListener: removeListener
         }
     });
